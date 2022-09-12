277076b40ccc808ebda2ab5523e378bd124e96cc
Irina Kasatkina <kasatkina235@gmail.com>
2022-09-12 19:41:51 +0300

    show Owner on Flat page in admin

--- property/admin.py
+++ property/admin.py
@@ -3,9 +3,16 @@ from django.contrib import admin
 from .models import Complaint, Flat, Owner
 
 
+class FlatsInline(admin.TabularInline):
+    model = Owner.flats_in_property.through
+    raw_id_fields = ('owner', 'flat')
+
+
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
-    list_display = ('address', 'owners_phonenumber', 'owner_pure_phone',
-                    'price', 'new_building', 'construction_year', 'town')
+    inlines = [FlatsInline]
+    list_display = ('address', 'price', 'new_building',
+                    'construction_year', 'town')
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     raw_id_fields = ('liked_by',)
@@ -13,14 +20,14 @@ class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address')
 
 
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('author', 'flat')
 
 
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
-    raw_id_fields = ('flats_in_property',)
-
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Owner, OwnerAdmin)
+    exclude = ['flats_in_property']
+    inlines = [FlatsInline]
+    list_display = ('fullname', 'phonenumber', 'pure_phone')
+    search_fields = ('fullname', 'pure_phone')
